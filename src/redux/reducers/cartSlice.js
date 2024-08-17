9023c9f0caa215688d2ea768e1ab464b8f10ecd1
MohammadMahdi Salmani <msas.mohammad1382@yahoo.com>
2024-08-18 02:02:31 +0330

    Manage Orders in the LocalStorage

--- src/redux/reducers/cartSlice.js
+++ src/redux/reducers/cartSlice.js
@@ -3,48 +3,62 @@ import { createSlice } from "@reduxjs/toolkit"
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
-        totalPrice: 0,
-        items: []
+        totalPrice: JSON.parse(localStorage.getItem("totalPrice")) ?? 0,
+        items: JSON.parse(localStorage.getItem("order")) ?? []
     },
     reducers: {
         increment(state, action) {
+            let food = {
+                name: action.payload.name,
+                price: action.payload.price,
+                img: action.payload.img,
+                count: 1
+            }
+
             if (state.items.length > 0) {
                 let item = state.items.filter(item => item.name === action.payload.name)
+                let order = JSON.parse(localStorage.getItem("order"))
+                let localStorageItem = order.filter(i => i.name === action.payload.name)
+
                 if(item[0]) {
                     item[0].count++
+                    localStorageItem[0].count++
+                    localStorage.setItem("order", JSON.stringify(order))
+                    
                     state.totalPrice = Number(state.totalPrice) + Number(item[0].price)
                 } else {
-                    state.items.push({
-                        name: action.payload.name,
-                        price: action.payload.price,
-                        img: action.payload.img,
-                        count: 1
-                    })
+                    state.items.push(food)
+                    order.push(food)
+                    localStorage.setItem("order", JSON.stringify(order))
 
                     state.totalPrice = Number(state.totalPrice) + Number(action.payload.price)
                 }
             } else {
-                state.items.push({
-                    name: action.payload.name,
-                    price: action.payload.price,
-                    img: action.payload.img,
-                    count: 1
-                })
+                state.items.push(food)
+                localStorage.setItem("order", JSON.stringify([food]))
 
                 state.totalPrice = Number(state.totalPrice) + Number(action.payload.price)
             }
 
             state.totalPrice = Math.trunc(state.totalPrice * 100) / 100
+            localStorage.setItem("totalPrice", state.totalPrice)
         },
 
         decrement(state, action) {
             state.items.filter(item => {
                 if(item.name === action.payload.name) {
+                    let order = JSON.parse(localStorage.getItem("order"))
+                    let localStorageItem = order.filter(i => i.name === action.payload.name)
+
                     if(item.count === 1) {
-                        state.items.splice(state.items.findIndex(item => item.name === action.payload.name), 1);
+                        order.splice(order.findIndex(i => i.name === action.payload.name), 1)
+                        localStorage.setItem("order", JSON.stringify(order))
+                        state.items.splice(state.items.findIndex(item => item.name === action.payload.name), 1)
                         state.totalPrice = Number(state.totalPrice) - Number(item.price)
                     } else {
                         item.count--
+                        localStorageItem[0].count--
+                        localStorage.setItem("order", JSON.stringify(order))
                         state.totalPrice = Number(state.totalPrice) - Number(item.price)
                     }
                     
@@ -54,6 +68,7 @@ const cartSlice = createSlice({
             })
 
             state.totalPrice = Math.trunc(state.totalPrice * 100) / 100
+            localStorage.setItem("totalPrice", state.totalPrice)
         }
     }
 })
